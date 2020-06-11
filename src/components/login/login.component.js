e77b70d7004cf3b8f26a5d44309b1cc582904fc8
Aishik Deb <debaishik14@gmail.com>
2020-06-12 02:59:43 +0530

    fix

--- src/components/login/login.component.js
+++ src/components/login/login.component.js
@@ -142,7 +142,7 @@ class Login extends Component{
                         <div className="img">
                             <div className="img__text m--up">
                                 <h2>New here?</h2>
-                                <p>Sign up and discover great amount of new opportunities!</p>
+                                <p>Sign up and discover great number of new friends!</p>
                             </div>
                             <div className="img__text m--in">
                                 <h2>One of us?</h2>
