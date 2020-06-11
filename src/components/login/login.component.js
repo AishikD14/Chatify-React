931618e510f25ca94269ab041920cd26c9ea8af5
Aishik Deb <debaishik14@gmail.com>
2020-06-12 02:26:40 +0530

    login functionality fix

--- src/components/login/login.component.js
+++ src/components/login/login.component.js
@@ -7,26 +7,30 @@ import { stateToProps, DispatchToProps } from '../../reducerfunctions';
 import './login.scss';
 
 class Login extends Component{
+    constructor(props){
+        super(props);
+
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onChangeUserName = this.onChangeUserName.bind(this);
+        this.onSubmitLogin = this.onSubmitLogin.bind(this);
+        this.onSubmitRegister = this.onSubmitRegister.bind(this);
+        this.clearVal = this.clearVal.bind(this);
+
+        this.state = {
+            email: "",
+            password: "",
+            name: ""
+        }
+    }
     componentDidMount(){
         document.querySelector('.img__btn').addEventListener('click', function () {
             document.querySelector('.cont').classList.toggle('s--signup');
         });
     }
-    constructor(props){
-        super(props);
-
-        this.onChangeUserName = this.onChangeUserName.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.state = {
-            username: "",
-            password: ""
-        }
-    }
-    onChangeUserName(e){
+    onChangeEmail(e){
         this.setState({
-            username: e.target.value
+            email: e.target.value
         })
     } 
     onChangePassword(e){
@@ -34,79 +38,103 @@ class Login extends Component{
             password: e.target.value
         })
     } 
-    onSubmit(e){
+    onChangeUserName(e){
+        this.setState({
+            name: e.target.value
+        })
+    }
+    clearVal(e){
+        this.setState({
+            email: "",
+            password: "",
+            name: ""
+        })
+    }
+    onSubmitLogin(e){
         e.preventDefault();
         const user = {
-            username: this.state.username,
+            email: this.state.email,
             password: this.state.password
         }
-        // console.log(user);
+        console.log(user);
 
-        axios.post("http://localhost:5000/users/login",user)
-            .then(res => {
-                console.log(res.data[0].username);
-                if(res.data.length === 1){
-                    if(user.username === "admin"){
-                        history.push('/exercise');
-                    }
-                    else{
-                        this.props.setUser(user.username);
-                        history.push('/exer_list_user');
-                    }
-                }
-                else{
-                    alert("Incorrect username or password");
-                }
-            })
-            .catch(function(error){
-                alert("Something went wrong");
-                console.log(error);
-            });
+        // axios.post("http://localhost:5000/users/login",user)
+        //     .then(res => {
+        //         console.log(res.data[0].username);
+        //         if(res.data.length === 1){
+        //             if(user.username === "admin"){
+        //                 history.push('/exercise');
+        //             }
+        //             else{
+        //                 this.props.setUser(user.username);
+        //                 history.push('/exer_list_user');
+        //             }
+        //         }
+        //         else{
+        //             alert("Incorrect username or password");
+        //         }
+        //     })
+        //     .catch(function(error){
+        //         alert("Something went wrong");
+        //         console.log(error);
+        //     });
         this.setState({
-            username: "",
+            email: "",
             password: ""
+        })
+    }
+    onSubmitRegister(e){
+        e.preventDefault();
+        const user = {
+            email: this.state.email,
+            password: this.state.password,
+            name: this.state.name
+        }
+        console.log(user);
+
+        // axios.post("http://localhost:5000/users/login",user)
+        //     .then(res => {
+        //         console.log(res.data[0].username);
+        //         if(res.data.length === 1){
+        //             if(user.username === "admin"){
+        //                 history.push('/exercise');
+        //             }
+        //             else{
+        //                 this.props.setUser(user.username);
+        //                 history.push('/exer_list_user');
+        //             }
+        //         }
+        //         else{
+        //             alert("Incorrect username or password");
+        //         }
+        //     })
+        //     .catch(function(error){
+        //         alert("Something went wrong");
+        //         console.log(error);
+        //     });
+        this.setState({
+            email: "",
+            password: "",
+            name: ""
         })
     }
 
     render(){
         return(
-            // <div className="text-center">
-            //     <h1 style={{marginTop:15+'px'}}>Welcome to your Personal Exercise Tracker</h1>
-            //     <br />
-            //     <br />
-            //     <h4>Enter your details below to login</h4>
-            //     <form onSubmit={this.onSubmit}>
-            //         <div className="form-group">
-            //             <label>Username</label>
-            //             <input type="text" required className="form-control" value={this.state.username} onChange={this.onChangeUserName} />
-            //         </div>
-            //         <div className="form-group">
-            //             <label>Password</label>
-            //             <input type="password" required className="form-control" value={this.state.password} onChange={this.onChangePassword} />
-            //         </div>
-            //         <div className="form-group">
-            //             <span>
-            //                 <input type="submit" value="Login" className="btn btn-primary" />
-            //                 <button  value="Register" className="btn btn-primary" style={{marginLeft:5+'px'}}><Link to={"/register"} style={{color:'#fff', textDecoration: 'none'}}>Register</Link></button>
-            //             </span>
-            //         </div>
-            //     </form>
-            // </div>
-            
             <div className="login-body" translate="no">
                 <div className="cont s--signup">
-                    <form className="form sign-in" onSubmit={this.onSubmit}>
+                    <form className="form sign-in" onSubmit={this.onSubmitLogin}>
                         <h2>Welcome back,</h2>
                         <label>
                             <span>Email</span>
-                            <input type="email" required value={this.state.username} onChange={this.onChangeUserName}/>
+                            <input type="email" required value={this.state.email} onChange={this.onChangeEmail}/>
                         </label>
                         <label>
                             <span>Password</span>
                             <input type="password" required value={this.state.password} onChange={this.onChangePassword}/>
                         </label>
                         <p className="forgot-pass">Forgot password?</p>
-                        <button type="button" className="submit">Sign In</button>
+                        <button type="submit" className="submit">Sign In</button>
                         <button type="button" className="fb-btn">Connect with <span>facebook</span></button>
                     </form>
                     <div className="sub-cont">
@@ -119,28 +147,28 @@ class Login extends Component{
                                 <h2>One of us?</h2>
                                 <p>If you already has an account, just sign in. We've missed you!</p>
                             </div>
-                            <div className="img__btn">
+                            <div className="img__btn" onClick={this.clearVal}>
                                 <span className="m--up">Sign Up</span>
                                 <span className="m--in">Sign In</span>
                             </div>
                         </div>
-                        <div className="form sign-up">
+                        <form className="form sign-up" onSubmit={this.onSubmitRegister}>
                             <h2>Time to feel like home,</h2>
                             <label>
                                 <span>Name</span>
-                                <input type="text" />
+                                <input type="text" required value={this.state.name} onChange={this.onChangeUserName}/>
                             </label>
                             <label>
                                 <span>Email</span>
-                                <input type="email" />
+                                <input type="email" required value={this.state.email} onChange={this.onChangeEmail}/>
                             </label>
                             <label>
                                 <span>Password</span>
-                                <input type="password" />
+                                <input type="password" required value={this.state.password} onChange={this.onChangePassword}/>
                             </label>
-                            <button type="button" className="submit">Sign Up</button>
+                            <button type="submit" className="submit">Sign Up</button>
                             <button type="button" className="fb-btn">Join with <span>facebook</span></button>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
