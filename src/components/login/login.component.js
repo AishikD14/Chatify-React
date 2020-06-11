4e9d9717026c5416ab3e653f837d7fc3ba0f3b37
Aishik Deb <debaishik14@gmail.com>
2020-06-11 23:50:23 +0530

    added login ui

--- src/components/login/login.component.js
+++ src/components/login/login.component.js
@@ -4,8 +4,14 @@ import { Link } from 'react-router-dom';
 import history from '../../history';
 import { connect } from 'react-redux';
 import { stateToProps, DispatchToProps } from '../../reducerfunctions';
+import './login.scss';
 
 class Login extends Component{
+    componentDidMount(){
+        document.querySelector('.img__btn').addEventListener('click', function () {
+            document.querySelector('.cont').classList.toggle('s--signup');
+        });
+    }
     constructor(props){
         super(props);
 
@@ -64,27 +70,79 @@ class Login extends Component{
 
     render(){
         return(
-            <div className="text-center">
-                <h1 style={{marginTop:15+'px'}}>Welcome to your Personal Exercise Tracker</h1>
-                <br />
-                <br />
-                <h4>Enter your details below to login</h4>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Username</label>
-                        <input type="text" required className="form-control" value={this.state.username} onChange={this.onChangeUserName} />
+            // <div className="text-center">
+            //     <h1 style={{marginTop:15+'px'}}>Welcome to your Personal Exercise Tracker</h1>
+            //     <br />
+            //     <br />
+            //     <h4>Enter your details below to login</h4>
+            //     <form onSubmit={this.onSubmit}>
+            //         <div className="form-group">
+            //             <label>Username</label>
+            //             <input type="text" required className="form-control" value={this.state.username} onChange={this.onChangeUserName} />
+            //         </div>
+            //         <div className="form-group">
+            //             <label>Password</label>
+            //             <input type="password" required className="form-control" value={this.state.password} onChange={this.onChangePassword} />
+            //         </div>
+            //         <div className="form-group">
+            //             <span>
+            //                 <input type="submit" value="Login" className="btn btn-primary" />
+            //                 <button  value="Register" className="btn btn-primary" style={{marginLeft:5+'px'}}><Link to={"/register"} style={{color:'#fff', textDecoration: 'none'}}>Register</Link></button>
+            //             </span>
+            //         </div>
+            //     </form>
+            // </div>
+            
+            <div className="login-body" translate="no">
+                <div className="cont s--signup">
+                    <div className="form sign-in">
+                        <h2>Welcome back,</h2>
+                        <label>
+                            <span>Email</span>
+                            <input type="email" />
+                        </label>
+                        <label>
+                            <span>Password</span>
+                            <input type="password" />
+                        </label>
+                        <p className="forgot-pass">Forgot password?</p>
+                        <button type="button" className="submit">Sign In</button>
+                        <button type="button" className="fb-btn">Connect with <span>facebook</span></button>
                     </div>
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input type="password" required className="form-control" value={this.state.password} onChange={this.onChangePassword} />
+                    <div className="sub-cont">
+                        <div className="img">
+                            <div className="img__text m--up">
+                                <h2>New here?</h2>
+                                <p>Sign up and discover great amount of new opportunities!</p>
+                            </div>
+                            <div className="img__text m--in">
+                                <h2>One of us?</h2>
+                                <p>If you already has an account, just sign in. We've missed you!</p>
+                            </div>
+                            <div className="img__btn">
+                                <span className="m--up">Sign Up</span>
+                                <span className="m--in">Sign In</span>
+                            </div>
+                        </div>
+                        <div className="form sign-up">
+                            <h2>Time to feel like home,</h2>
+                            <label>
+                                <span>Name</span>
+                                <input type="text" />
+                            </label>
+                            <label>
+                                <span>Email</span>
+                                <input type="email" />
+                            </label>
+                            <label>
+                                <span>Password</span>
+                                <input type="password" />
+                            </label>
+                            <button type="button" className="submit">Sign Up</button>
+                            <button type="button" className="fb-btn">Join with <span>facebook</span></button>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <span>
-                            <input type="submit" value="Login" className="btn btn-primary" />
-                            <button  value="Register" className="btn btn-primary" style={{marginLeft:5+'px'}}><Link to={"/register"} style={{color:'#fff', textDecoration: 'none'}}>Register</Link></button>
-                        </span>
-                    </div>
-                </form>
+                </div>
             </div>
         );
     }
