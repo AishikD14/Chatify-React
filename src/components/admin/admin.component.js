480ca0c7d293e23169ad6ae50ac2c46f45376060
Aishik Deb <debaishik14@gmail.com>
2021-03-31 02:43:20 +0530

    refresh and clear message function integrated

--- src/components/admin/admin.component.js
+++ src/components/admin/admin.component.js
@@ -79,6 +79,40 @@ const Admin = () => {
             });
     }
 
+    const clearMessage = (room) => {
+        let payload = {
+            "room": room.roomId
+        }
+        setModal(true);
+        axios.post(custom.URL + "/admin/clear_message", payload, custom.options)
+            .then((res) => {
+                setModal(false);
+                if(res.status === 200){
+                    console.log("Messages cleared");
+                    let list = [];
+                    list = roomList.map((item) => {
+                        if(item.roomId === room.roomId){
+                            return {
+                                roomId: item.roomId,
+                                users: item.users,
+                                type: item.type,
+                                messageNo: 0
+                            }
+                        }
+                        return item;
+                    });
+                    setRoomList(list);
+                }
+                else{
+                    console.log("No such room present");
+                }
+            })
+            .catch((err) => {
+                setModal(false);
+                console.log(err);
+            });
+    }
+
     return(
         <div className="admin-body">
             <h1>Admin Portal</h1>
@@ -102,7 +136,7 @@ const Admin = () => {
                                 <td>{room.users}</td>
                                 <td>{room.type}</td>
                                 <td>{room.messageNo}</td>
-                                <td>Clear All</td>
+                                <td onClick={() => {clearMessage(room)}}>Clear All</td>
                                 <td onClick={() => {deleteRoom(room)}}>Delete Room</td>
                             </tr>
                         )
@@ -110,6 +144,7 @@ const Admin = () => {
                 </tbody>
             </table>
             <button type="button" className="btn btn-primary" onClick={logout}>Logout</button>
+            <button type="button" className="btn btn-success" onClick={getRoomList}>Refresh</button>
         </div>
     )
 }
